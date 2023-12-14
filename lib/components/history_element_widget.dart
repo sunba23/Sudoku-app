766ea8b85dec3a937a21943ea7afbde08fe05ab5
Franciszek Suszko <84635502+sunba23@users.noreply.github.com>
2023-12-15 00:42:14 +0100

    Performance focused grid display (#5)
    * performance based history grid creation
    
    * widget ui

--- lib/components/history_element_widget.dart
+++ lib/components/history_element_widget.dart
@@ -1,4 +1,4 @@
-import 'package:app/components/grid_widget.dart';
+import 'package:app/components/history_grid_widget.dart';
 import 'package:app/models/history_element.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -15,6 +15,20 @@ class _HistoryElementWidgetState extends State<HistoryElementWidget> {
   DateTime now = DateTime.now();
   String differenceString = '';
 
+  List<HistoryGridWidget> previewGrids = [];
+  List<HistoryGridWidget> dialogGrids = [];
+  Future<void>? loadGridsFuture;
+
+  Future<void> loadGrids() async {
+    previewGrids
+      ..add(HistoryGridWidget(height: 140, width: 140, puzzleString: widget.historyElement.inputSudokuString,))
+      ..add(HistoryGridWidget(height: 140, width: 140, puzzleString: widget.historyElement.outputSudokuString,));
+
+    dialogGrids
+      ..add(HistoryGridWidget(height: 260, width: 260, puzzleString: widget.historyElement.inputSudokuString,))
+      ..add(HistoryGridWidget(height: 260, width: 260, puzzleString: widget.historyElement.outputSudokuString,));
+  }
+
   @override
   void initState() {
     super.initState();
@@ -29,10 +43,10 @@ class _HistoryElementWidgetState extends State<HistoryElementWidget> {
     } else {
       differenceString = '${diff.inSeconds} seconds ago';
     }
+    loadGridsFuture = loadGrids();
   }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget buildWidget(BuildContext context) {
     return Dismissible(
       // key: Key(widget.historyElement.hashCode.toString()),
       key: UniqueKey(),
@@ -62,26 +76,29 @@ class _HistoryElementWidgetState extends State<HistoryElementWidget> {
             context: context,
             builder: (BuildContext context) {
               return AlertDialog(
-                title: const Text('Sudoku'),
+                title: const Center(child: Text('Sudoku')),
+                titleTextStyle: GoogleFonts.nunito(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 25,
+                  color: const Color.fromARGB(255, 57, 64, 83),
+                ),
                 content: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.7,
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height * 0.65,
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width * 0.85,
                   child: Column(
                     children: [
-                      SudokuGrid(
-                        height: 275,
-                        width: 275,
-                        puzzleString: widget.historyElement.inputSudokuString,
-                        isEditable: false,
-                        onChanged: (xd) {},
-                      ),
-                      SizedBox(height: MediaQuery.of(context).size.height * 0.05,),
-                      SudokuGrid(
-                        height: 275,
-                        width: 275,
-                        puzzleString: widget.historyElement.outputSudokuString,
-                        isEditable: false,
-                        onChanged: (xd) {},
-                      ),
+                      dialogGrids[0],
+                      SizedBox(height: MediaQuery
+                          .of(context)
+                          .size
+                          .height * 0.05,),
+                      dialogGrids[1],
                     ],
                   ),
                 ),
@@ -105,27 +122,47 @@ class _HistoryElementWidgetState extends State<HistoryElementWidget> {
                     color: const Color.fromARGB(255, 57, 64, 83),
                   ),
                 ),
-                const SizedBox(height: 10),
-                Text(
-                  'input sudoku: ${widget.historyElement.inputSudokuString}',
-                  style: GoogleFonts.nunito(
-                    fontWeight: FontWeight.w600,
-                    color: const Color.fromARGB(255, 57, 64, 83),
-                  ),
+                const SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    previewGrids[0],
+                    const Icon(
+                      Icons.arrow_forward_rounded,
+                      color: Color.fromARGB(255, 57, 64, 83),
+                      size: 20,
+                    ),
+                    previewGrids[1],
+                  ]
                 ),
-                const SizedBox(height: 10),
-                Text(
-                  'solved sudoku: ${widget.historyElement.outputSudokuString}',
-                  style: GoogleFonts.nunito(
-                    fontWeight: FontWeight.w600,
-                    color: const Color.fromARGB(255, 57, 64, 83),
-                  ),
-                ),
+                const SizedBox(height: 10,),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: loadGridsFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return buildWidget(context);
+        } else {
+          return Container(
+            decoration: BoxDecoration(
+              color: const Color (0xffebebf5),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 }
